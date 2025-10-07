3df594ce5605c3d95fbd400d679c05432a053cbf
muhamadazis32 <muhamadazis9a@gmail.com>
2025-10-07 11:49:02 +0700

    Feat: penambahan secret key

--- src/main/java/org/iconpln/master_unitap/controller/MasterUnitApController.java
+++ src/main/java/org/iconpln/master_unitap/controller/MasterUnitApController.java
@@ -1,6 +1,9 @@
 package org.iconpln.master_unitap.controller;
 
+import io.smallrye.common.constraint.NotNull;
 import jakarta.inject.Inject;
+import jakarta.validation.constraints.NotBlank;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -11,6 +14,7 @@ import org.iconpln.master_unitap.service.MasterUnitApService;
 import org.iconpln.util.ErrorResponse;
 import org.iconpln.util.PagedResultDto;
 import org.iconpln.util.ResponseModel;
+import org.mindrot.jbcrypt.BCrypt;
 
 @Slf4j
 @Path("/api/unitap")
@@ -22,11 +26,28 @@ public class MasterUnitApController {
     @Inject
     MasterUnitApService service;
 
+    @Inject
+    @ConfigProperty(name = "app.secret.key")
+    String secretKeyNewAp2t;
+
     @GET
     public Response getAll(
             @QueryParam("page") @DefaultValue("1") int page,
-            @QueryParam("size") @DefaultValue("10") int size) {
+            @QueryParam("size") @DefaultValue("10") int size,
+            @NotBlank @HeaderParam("X-Secret-Key") String header_key) {
         try {
+
+            // cek header_key
+            boolean checkHeader = BCrypt.checkpw(secretKeyNewAp2t, header_key);
+            if (!checkHeader) {
+                return Response.status(Response.Status.UNAUTHORIZED)
+                        .entity(new ResponseModel("UNAUTHORIZED",
+                                false,
+                                401,
+                                null))
+                        .build();
+            }
+
             log.info("REST: GET /api/unitap?page={}&size={}", page, size);
 
             PagedResultDto<MasterUnitAp> dataUnitAp = service.getAll(page, size);
